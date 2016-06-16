fad072dce1d0f4e2cbf79c5d5865402b84a188cc
Alexander <abcconsultingit@gmail.com>
2016-06-16 15:09:23 -0400

    initial check in

---
+++ smartapps/abthere/close-garage-door.src/close-garage-door.groovy
@@ -0,0 +1,44 @@
+definition(
+    name: "Close Garage Door",
+    namespace: "abthere",
+    author: "abthere",
+    description: "Close Garage Door at certain time.",
+    category: "Safety & Security",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/light_outlet@2x.png"
+)
+
+preferences { 
+    section("Select Garage Door Sensor...") {
+		input name: "sensor", type: "capability.contactSensor", title: "Which sensor?", required: true, multiple: false
+	}
+    section("Select Garage door...") {
+		input name: "garageDoor", type: "capability.garageDoorControl", title: "Which door?", required: true, multiple: false
+	}
+	section("Check them all on at...") {
+		input name: "startTime", title: "Turn On Time?", type: "time"
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	schedule(startTime, "startTimerCallback")
+}
+
+def updated(settings) {
+	unschedule()
+	schedule(startTime, "startTimerCallback")
+}
+def startTimerCallback() {
+    if ("closed" != sensor.currentContact) {
+    	if ("closed" == garageDoor.currentDoor) {
+        	log.debug "Opening ${garageDoor.displayName}"
+    		garageDoor.open()
+            log.debug "Will close ${garageDoor.displayName} in 60 seconds"
+            garageDoor.close(delay:6000)
+        } else {
+            log.debug "Closing ${garageDoor.displayName}"
+            garageDoor.close()
+        }
+   	}
+}
